0c699d073e18c210aa626e854578d9d3fd052cf2
Scott Tracy <scott.tracy.st@gmail.com>
2018-06-21 15:47:51 -0500

    created project, added contestant and sweepstakes class

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        public string firstName = "";
+        public string lastName;
+        public string email;
+        public int registration;
+        public Dictionary<string, string> info = new Dictionary<string, string>();
+
+        public string GetUserInput(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+        public void GetFirstName()
+        {
+            firstName = GetUserInput("What is your first name?");
+        }
+        public void GetLastName()
+        {
+            lastName = GetUserInput("What is your last name?");
+        }
+        public void GetEmail()
+        {
+            email = GetUserInput("What is your E-mail address?");
+        }
+        public void GetInfo()
+        {
+            GetFirstName();
+            GetLastName();
+            GetEmail();
+        }
+        public void MakeDictionary()
+        {
+            info.Add("First name", firstName);
+            info.Add("Last name", lastName);
+            info.Add("E-mail", email);
+            info.Add("Registration number", registration.ToString());
+        }
+
+
+        
+    }
+}
---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        public Dictionary<int,Contestant> contestants = new Dictionary<int,Contestant>();
+        public int registrationNumber=1;
+        string name;
+        public Sweepstakes(String name)
+        {
+            this.name = name;
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+            contestant.GetInfo();
+            contestants.Add(registrationNumber, contestant);
+            contestant.registration = registrationNumber;
+            registrationNumber++;
+        }
+        public string PickWinner()
+        {
+            Random random = new Random();
+            int winningNumber = random.Next(1, registrationNumber - 1);
+            Contestant winner = contestants[winningNumber];
+            return winner.firstName + " " + winner.lastName;
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            contestant.MakeDictionary();
+            foreach (KeyValuePair<string, string> identity in contestant.info)
+            {
+                Console.WriteLine(identity.Key + ": " + identity.Value);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
